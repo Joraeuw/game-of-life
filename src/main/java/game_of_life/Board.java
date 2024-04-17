b0f902f4a8d5aca1cb99e0646361526bd70d29e9
zhora-nersisyan <zhora.nersisyan@quanterall.com>
2024-04-17 17:25:26 +0300

    remove convolution class

--- src/main/java/game_of_life/Board.java
+++ src/main/java/game_of_life/Board.java
@@ -2,7 +2,6 @@ package game_of_life;
 
 import game_of_life.types.Tribe;
 import game_of_life.utils.Color;
-import game_of_life.utils.Convolution;
 import game_of_life.utils.KMeansClustering;
 import game_of_life.utils.Pair;
 import game_of_life.utils.Point;
@@ -54,10 +53,6 @@ public class Board {
     printBoard();
     CompletableFuture.runAsync(this::prepNextState);
     sleep();
-  }
-
-  private void prepNextState() {
-    backBuffer = Convolution.apply(frontBuffer);
   }
 
   public void printBoard() {
@@ -116,5 +111,53 @@ public class Board {
     System.out.print("\033[H\033[2J");
     System.out.flush();
   }
+
+    private void prepNextState() {
+      Random random = new Random();
+      int rows_l = frontBuffer.length;
+      int cols_l = frontBuffer[0].length;
+      Vector<Integer> rows = Vector.range(0, rows_l);
+      Vector<Integer> cols = Vector.range(0, cols_l);
+  
+      rows.forEach(row -> cols.forEach(col -> {
+        Tribe currentTribe = frontBuffer[row][col];
+        Vector<Pair<Tribe, Integer>> tribe_aliveCounts = this.countTribeNeighbors(row, col);
+        int countLivingNeighbors = tribe_aliveCounts.foldLeft(0, (acc, pair) -> acc + pair.getSecond());
+  
+        if (Tribe.isAlive(currentTribe) && (countLivingNeighbors < 2 ||
+            countLivingNeighbors > 3)) {
+          this.backBuffer[row][col] = Tribe.NONE;
+        } else if (Tribe.isAlive(currentTribe)) {
+          Pair<Tribe, Integer> tmp = tribe_aliveCounts.find(pair -> pair.getFirst() == currentTribe)
+              .getOrElse(Pair.of(currentTribe, -1));
+          if (tmp.getSecond() == 2 || tmp.getSecond() == 3) {
+            this.backBuffer[row][col] = currentTribe;
+          } else if (random.nextDouble() <= .5d) {
+            this.backBuffer[row][col] = currentTribe;
+          } else {
+            this.backBuffer[row][col] = Tribe.NONE;
+          }
+        } else if (!Tribe.isAlive(currentTribe)) {
+          Pair<Tribe, Integer> tmp = tribe_aliveCounts
+              .find(pair -> pair.getSecond() == 3).getOrElse(Pair.of(Tribe.NONE, -1));
+              this.backBuffer[row][col] = tmp.getFirst();
+        }
+      }));
+    }
+
+    private Vector<Pair<Tribe, Integer>> countTribeNeighbors(int row, int col) {
+      Vector<Tribe> neighbors = Vector.rangeClosed(-1, 1)
+          .flatMap(offsetRow -> Vector.rangeClosed(-1, 1).map(offsetCol -> Point.from(row + offsetRow, col + offsetCol)))
+          .reject(point -> point.getX() - row == 0 && point.getY() - col == 0)
+          .filter(point -> point.getX() >= 0 &&
+              point.getX() < frontBuffer.length &&
+              point.getY() >= 0 &&
+              point.getY() < frontBuffer[0].length)
+          .map(point -> frontBuffer[point.getX()][point.getY()])
+          .filter(tribe -> tribe != Tribe.NONE);
+
+      return Vector.of(Tribe.values())
+          .map(tribe -> Pair.of(tribe, neighbors.filter(curr_tribe -> curr_tribe == tribe).size()));
+    }
 
 }
